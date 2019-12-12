48400fb8dba0cf7b733b38f9146283af46c52016
tardyparty <tardyparty@users.noreply.github.com>
2019-12-11 20:52:15 -0600

    Updates

---
+++ precache-manifest.75508740ffbcc920a72db528bd325a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87f77fc35f722026e120d9d37f9e3f7",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "24e2922edeac52077c7c",
+    "url": "/clickyGame/static/css/main.e66c7554.chunk.css"
+  },
+  {
+    "revision": "950f4f7890df28b4d4de",
+    "url": "/clickyGame/static/js/2.b2383e26.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickyGame/static/js/2.b2383e26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24e2922edeac52077c7c",
+    "url": "/clickyGame/static/js/main.fe09616d.chunk.js"
+  },
+  {
+    "revision": "ede2a11c07c1635130fe",
+    "url": "/clickyGame/static/js/runtime-main.51c15a7c.js"
+  }
+]);
